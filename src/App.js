67cf9418dab23ae7b1590ba61d6110c72c79f59b
ouniemna <ouni.emna@hotmail.fr>
2020-01-05 22:54:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Filter from "./Filter";
+import MovieList from "./MovieList";
+import AddModal from "./AddModal";
+import {connect} from 'react-redux';
+import {searchMovie} from './actions/movieActions'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./styles.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+  
+      newRating: 1,
+      keyword: ""
+    };
+  }
+
+  rateMovie = stars => {
+    this.setState({
+      newRating: stars
+    });
+  };
+  searchmovie = movieName => {
+    this.setState({
+      keyword: movieName
+    });
+  };
+  searchMovieBy = () => {
+   
+    this.props.searchMovie(this.state.keyword,this.state.newRating)
+  };
+
+ 
+
+  render() {
+    return (
+      <div className="App">
+        <Filter
+          searchIt={word => this.searchmovie(word)}
+          rateIt={num => this.rateMovie(num)}
+        />
+        <AddModal />
+        <MovieList editMode='false'/>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
